3ea0d192b0df8937166e68b32e4432667b396dfb
IlonaStr <hellosidney17@gmail.com>
2020-07-01 14:34:24 +0300

    fixed 5,6,7

--- task-5.js
+++ task-5.js
@@ -1,11 +1,5 @@
 'use strict';
 
-// const inputName = document.querySelector('#name-input');
-// const outputName = document.querySelector('#name-output');
-// inputName.addEventListener('input', function(event) {
-//     outputName.textContent = inputName.value === '' ? 'незнакомец' : inputName.value
-// })
-
 const inputName = document.querySelector('#name-input');
 const outputName = document.querySelector('#name-output');
 inputName.addEventListener('input', ({target}) => 
